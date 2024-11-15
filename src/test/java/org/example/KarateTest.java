452cf5e7ff79b01b6631365e47817ea799aa6767
Anxo Portela-Insua Blanco <hello@anxoportela.dev>
2024-11-15 17:15:46 +0100

    update project

--- src/test/java/org/example/KarateTest.java
+++ src/test/java/org/example/KarateTest.java
@@ -4,13 +4,8 @@ import com.intuit.karate.junit5.Karate;
 
 public class KarateTest {
 
-    /**
-     * Este método ejecuta ambos archivos .feature en una única ejecución de prueba
-     * y genera un solo reporte con los resultados de ambas características.
-     */
     @Karate.Test
     Karate testAllApi() {
-        // Se especifican ambos archivos *.feature que se desean ejecutar
-        return Karate.run("src/test/resources/api-test.feature").reportDir("report");
+        return Karate.run("classpath:api-test.feature").reportDir("target/karate-report");
     }
 }
